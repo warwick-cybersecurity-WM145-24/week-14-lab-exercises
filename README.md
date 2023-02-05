0463d7a3f75ef62283d2a454cfd12441e1c381f3
Jujhar Singh <jujhar@jujhar.com>
2023-02-05 22:03:04 +0000

    adds some mashup javascript

---
+++ README.md
@@ -0,0 +1,7 @@
+# Week 14
+
+CI and CD
+
+## Project structure
+
+- `/insecure` a nodejs project with insecure dependencies
--- insecure/index.js
+++ insecure/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const unusedVar = 'This var is unused'
 
 app.get('/', function (req, res) {
+  if (potato=true){ console.log('true'  ) }
   res.send('Hello World');
 })
 
